100e2c596e99b50a8a3a48126d1b0a807301f444
giannagalard <giannagalard@gmail.com>
2021-12-26 13:16:25 -0500

    FEAT: BACKEND WORKS WOOOWEEEEE

--- gql/User.js
+++ gql/User.js
@@ -1,7 +1,10 @@
 const { gql } = require("apollo-server"); // if throws error fix this
 const { date } = require("../gql/Date");
+const bcrypt = require('bcryptjs'); // encrypt passwords
 
-const userDefs = gql`
+// define the schema, the models, the queries, and the mutations for the graphql server
+// create user, update user, and delete user
+const userDefs = gql` 
   scalar Date
   type User {
     id: Int!
@@ -16,18 +19,36 @@ const userDefs = gql`
   type Query {
     user(id: Int!): User
   }
+  type Mutation {
+    createUser(firstname: String!, lastname: String!, email: String!, password: String!): User!
+  }
 `;
 
+
 const userResolvers = {
-  Query: {
-    user: (parent, args, context, info) => {
-      return context.models.User.findOne({
-        where: {
-          id: args.id,
+    Query: {
+        user: (parent, args, context, info) => {
+            return context.models.User.findOne({
+                where: {
+                    id: args.id,
+                },
+            });
         },
-      });
     },
-  },
+
+    Mutation: {
+        createUser: async (parent, args, context, info) => { 
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(args.password, salt);
+            const user = await context.models.User.create({
+                firstname: args.firstname,
+                lastname: args.lastname,
+                email: args.email,
+                password: hashedPassword,
+            });
+            return user;
+        },
+    },
 };
 
 module.exports = { userDefs, userResolvers };
