84fd73150f890d4cc39dbb8f3b088f5649484479
AndreaHabib <andrea.atef@yahoo.com>
2021-12-27 13:16:37 -0500

    FEAT: Added gzip compression

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require("http-errors");
+var compression = require("compression");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -9,6 +10,7 @@ var cors = require("cors");
 // console.log(process.env);
 app.use(cors());
 
+app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
