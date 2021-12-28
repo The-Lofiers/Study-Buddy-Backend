6f9bf7a8118a960a753f58af151158a242ea0af9
giannagalard <giannagalard@gmail.com>
2021-12-28 17:42:49 -0500

    FEAT: Generated class models

--- gql/Class.js
+++ gql/Class.js
@@ -60,3 +60,4 @@ const classDef = gql`
         ): Boolean!
     }
 `;
+
---
+++ migrations/20211228224123-create-class.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Classes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      className: {
+        type: Sequelize.STRING
+      },
+      todo_ID: {
+        type: Sequelize.INTEGER
+      },
+      notes_ID: {
+        type: Sequelize.INTEGER
+      },
+      grades_ID: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Classes');
+  }
+};
