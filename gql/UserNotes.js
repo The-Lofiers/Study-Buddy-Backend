4ad1207f435b7f78d7fd6842a6c7c55e41d202a9
giannagalard <giannagalard@gmail.com>
2022-01-10 17:31:16 -0500

    FEAT: usernotes query and muatation

--- gql/UserNotes.js
+++ gql/UserNotes.js
@@ -1,7 +1,35 @@
 const { gql, UserInputError, AuthenticationError } = require("apollo-server");
 const jwt = require("jsonwebtoken");
 
-const classDef = gql`
+const userNotesDef = gql`
     scalar Date
 
-    
+    type UserNotes {
+        id: Int!
+        notes_ID: Int!
+        url: String!
+        docName: String!
+        updatedAt: Date!
+        createdAt: Date!
+    }
+
+    type Query {
+        userNotes(id: Int!): UserNotes!
+    }
+
+    type Mutation {
+        createUserNotes(
+            notes_ID: Int!,
+            url: String!,
+            docName: String!
+        ): UserNotes!
+
+        editUserNotes(
+            url: String,
+            docName: String
+        ): UserNotes!
+
+        deleteUserNotes(
+            id: Int!
+        ): Boolean!
+`;
