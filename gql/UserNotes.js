2be0b701eddffe846f2f8c6b6f2104eda713b231
giannagalard <giannagalard@gmail.com>
2022-01-10 17:45:32 -0500

    FEAT: Added usernotes resolvers UwU

--- gql/UserNotes.js
+++ gql/UserNotes.js
@@ -33,3 +33,68 @@ const userNotesDef = gql`
             id: Int!
         ): Boolean!
 `;
+
+const userNotesResolvers = {
+    Query: {
+        userNotes: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.UserNotes.findOne({
+                where: {
+                    id: args.id,
+                },
+            });
+        },
+    },
+    Mutation: {
+        createUserNotes: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.UserNotes.create({
+                notes_ID: args.notes_ID,
+                url: args.url,
+                docName: args.docName,
+            });
+        },
+        editUserNotes: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.UserNotes.update(
+                {
+                    url: args.url,
+                    docName: args.docName,
+                },
+                {
+                    where: {
+                        id: args.id,
+                    },
+                }
+            );
+        },
+        deleteUserNotes: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.UserNotes.destroy({
+                where: {
+                    id: args.id,
+                },
+            });
+        },
+    },
+};
