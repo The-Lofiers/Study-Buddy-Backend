23dbf65dd906eaf7a9c85bf2499cb4949470495d
giannagalard <giannagalard@gmail.com>
2022-01-10 18:14:34 -0500

    generated class model with sequelize

---
+++ migrations/20220110231012-create-class.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('classes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      className: {
+        type: Sequelize.STRING
+      },
+      todo_ID: {
+        type: Sequelize.INTEGER
+      },
+      notes_ID: {
+        type: Sequelize.INTEGER
+      },
+      grade_ID: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('classes');
+  }
+};
