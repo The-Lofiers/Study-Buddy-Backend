f11e7d3b1738c3f88bb56dfd9b2da7e7d2240c8c
giannagalard <giannagalard@gmail.com>
2022-01-10 18:04:21 -0500

    FEAT: ToDo resolvers

--- gql/ToDo.js
+++ gql/ToDo.js
@@ -30,3 +30,60 @@ const toDoDef = gql`
             id: Int!
         ): Boolean!
 `;
+
+const toDoResolvers = {
+    Query: {
+        toDo: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.ToDo.findOne({
+                where: {
+                    id: args.id,
+                },
+            });
+        },
+    },
+
+    Mutation: {
+        createToDo: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.ToDo.create({
+                toDo: args.todo,
+                class_ID: args.class_ID,
+            });
+        },
+        editToDo: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.ToDo.update({
+                toDo: args.todo,
+            });
+        },
+        deleteToDo: (parent, args, context, info) => {
+            if (!context.user) {
+                // same context used to check if user is logged in
+                throw new AuthenticationError(
+                    "OOPSIE WOOPSIE UWU you are not authenticated!"
+                );
+            }
+            return context.models.ToDo.destroy({
+                where: {
+                    id: args.id,
+                },
+            });
+        },
+    },
+};
