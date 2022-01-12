8f7db80db31bdb7b9bf6ee2344bafc286f81ed18
AndreaHabib <andrea.atef@yahoo.com>
2022-01-11 22:54:40 -0500

    FEAT: unit test working for creating user

--- test/User.test.js
+++ test/User.test.js
@@ -2,18 +2,11 @@
 
 const chai = require("chai");
 const faker = require("faker");
-const EasyGraphQLTester = require("easygraphql-tester");
-const { userDefs, userResolvers } = require("../gql/User");
-const { assert } = require("chai");
+const url = `http://localhost:4000/`;
+const request = require("supertest")(url);
 const expect = chai.expect;
 
 describe("Test User GraphQL queries and mutations", () => {
-  let tester;
-
-  before(() => {
-    tester = new EasyGraphQLTester(userDefs, userResolvers);
-  });
-
   it("should create a user", async () => {
     const user = {
       firstName: faker.name.firstName(),
@@ -31,7 +24,21 @@ describe("Test User GraphQL queries and mutations", () => {
                     )
                   }
             `;
-    const result = await tester.mock(mutation);
-    expect(result.data.createUser).to.be.an("string");
+    request
+      .post("graphql")
+      .send({
+        query: mutation,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .expect(200) // status code that you expect to be returned
+      .end(function (error, response) {
+        if (error) console.log(error);
+        expect(response.body.errors).to.be.undefined;
+        expect(response.body.data.createUser).to.be.a("string");
+        expect(response.body.data.createUser).to.not.be.empty;
+        expect(response.body.data.createUser.length).to.be.equal(105);
+      });
   });
 });
