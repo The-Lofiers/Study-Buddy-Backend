ae4f68f5cda87b33bf6f0b0d65a91a8d1aa70456
AndreaHabib <andrea.atef@yahoo.com>
2022-01-12 10:46:50 -0500

    FEAT: unit test to create and then delete user

--- test/User.test.js
+++ test/User.test.js
@@ -7,15 +7,14 @@ const request = require("supertest")(url);
 const expect = chai.expect;
 
 describe("Test User GraphQL queries and mutations", () => {
-  it("should create a user, get a user", async () => {
-    let token = "";
-    let user_id = 0;
+  it("should create a user, and delete user", async () => {
     const user = {
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       email: faker.internet.email(),
       password: faker.internet.password(),
     };
+
     const mutation = `
                   mutation {
                     createUser(
@@ -43,46 +42,30 @@ describe("Test User GraphQL queries and mutations", () => {
       .expect(200) // status code that you expect to be returned
       .end(function (error, response) {
         if (error) console.log(error);
-        const query = `
-        query {
-          user(id: ${response.body.data.createUser.user.id}) {
-            id
-            firstname
-            lastname
-            email
-            password
-            createdAt
-            updatedAt
-          }
-        }
-  `;
-        request
-          .post("graphql")
-          .set("Authorization", response.body.data.createUser.token)
-          .send({
-            query: query,
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-          .expect(200) // status code that you expect to be returned
-          .end(function (error, response) {
-            if (error) console.log(error);
-            console.log(response.body.data.user);
-            expect(response.body.errors).to.be.undefined;
-            expect(response.body.data.user.id).to.be.a("number");
-            expect(response.body.data.user.firstname).to.be.a("string");
-            expect(response.body.data.user.lastname).to.be.a("string");
-            expect(response.body.data.user.email).to.be.a("string");
-            expect(response.body.data.user.password).to.be.a("string");
-            expect(response.body.data.user.createdAt).to.be.a("string");
-            expect(response.body.data.user.updatedAt).to.be.a("string");
-          });
         expect(response.body.errors).to.be.undefined;
         expect(response.body.data.createUser.token).to.be.a("string");
         expect(response.body.data.createUser.user.id).to.be.a("number");
         expect(response.body.data.createUser.token).to.not.be.empty;
         expect(response.body.data.createUser.token.length).to.be.equal(105);
+
+        const deleteUser = `
+            mutation {
+              deleteUser(id: ${response.body.data.createUser.user.id})
+            }`;
+
+        request
+          .post("graphql")
+          .set("Authorization", `${response.body.data.createUser.token}`)
+          .send({
+            query: deleteUser,
+            headers: { "Content-Type": "application/json" },
+          })
+          .expect(200)
+          .end(function (error, response) {
+            if (error) console.log(error);
+            expect(response.body.data.deleteUser).to.be.true;
+            expect(response.body.errors).to.be.undefined;
+          });
       });
   });
 });
