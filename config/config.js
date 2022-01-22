a880698928d9c839c1d87975e5cdfabe23874337
AndreaHabib <andrea.atef@yahoo.com>
2022-01-22 13:33:53 -0500

    FEAT: added prod and ssl

--- config/config.js
+++ config/config.js
@@ -15,10 +15,16 @@ module.exports = {
     dialect: process.env.DIALECT,
   },
   production: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
+    username: process.env.PRO_DB_USER,
+    password: process.env.PRO_PASSWORD,
+    database: process.env.PRO_DATABASE,
+    host: process.env.PRO_HOST,
+    dialect: process.env.PRO_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false, // This line will fix new error
+      },
+    },
   },
 };
--- models/index.js
+++ models/index.js
@@ -12,39 +12,39 @@ const db = {};
 // BELONGS TO MANY = MANY TO MANY
 let sequelize; // = new Sequelize(config.database, config.username, config.password, config);
 if (config.use_env_variable) {
-    // if we are using the environment variable
-    sequelize = new Sequelize(process.env[config.use_env_variable], config); // connect to the database
+  // if we are using the environment variable
+  sequelize = new Sequelize(process.env[config.use_env_variable], config); // connect to the database
 } else {
-    sequelize = new Sequelize(
-        config.database,
-        config.username,
-        config.password,
-        config
-    ); // connect to the database
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  ); // connect to the database
 }
 fs.readdirSync(__dirname) // read the directory
-    .filter((file) => {
-        // filter the files
-        // if the file is not a hidden file, and not the current file, and is a .js file
-        return (
-            file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-        );
-    })
-    .forEach((file) => {
-        // for each file
-        // require the file and pass the sequelize and datatypes
-        const model = require(path.join(__dirname, file))(
-            sequelize,
-            Sequelize.DataTypes
-        );
-        db[model.name] = model; // add the model to the db object
-    });
+  .filter((file) => {
+    // filter the files
+    // if the file is not a hidden file, and not the current file, and is a .js file
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    // for each file
+    // require the file and pass the sequelize and datatypes
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model; // add the model to the db object
+  });
 Object.keys(db).forEach((modelName) => {
-    // for each model
-    if (db[modelName].associate) {
-        // if the model has an associate function
-        db[modelName].associate(db); // call the associate function
-    }
+  // for each model
+  if (db[modelName].associate) {
+    // if the model has an associate function
+    db[modelName].associate(db); // call the associate function
+  }
 });
 db.sequelize = sequelize; // add the sequelize object to the db object
 db.Sequelize = Sequelize; // add the Sequelize object to the db object
